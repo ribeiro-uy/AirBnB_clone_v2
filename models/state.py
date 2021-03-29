550f850cd1c8766788dbfe85c216ab581cba6f9a
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-29 14:03:13 -0300

    update to mysql

--- models/state.py
+++ models/state.py
@@ -4,9 +4,34 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
+from models.engine.db_storage import DBStorage
+from os import getenv
+from sqlalchemy.orm import relationship
+from models.city import City
+from models import storage
+HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+
+    if HBNB_TYPE_STORAGE == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        # If State object is deleted, all linked City objects must be automatically deleted.
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+    if HBNB_TYPE_STORAGE != "db":
+        @property
+        def cities(self):
+            """
+            Getter attribute to list City instaces
+            """
+            new_list = []
+            all_cities = storage.__objects.all(City):
+            for city in all_cities:
+                if city.state_id == self.id:
+                    new_list.append(city)
+            return new_list
