28b6b40e8f91cfa51539051f421f0776bfafaebc
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-29 14:02:31 -0300

    New engine DBStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,29 +1,83 @@
 #!/usr/bin/python3
-"""This module defines new engine DBStorage"""
+"""
+This module defines new engine DBStorage
+"""
+
+from os import getenv
+from sqlalchemy.orm import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+from models.state import State
+from models.city import City
+from models.base_model import BaseModel, Base
 
 class DBStorage:
-    """Class definition"""
-
-    HBNB_MYSQL_USER = 'hbnb_dev'
-    HBNB_MYSQL_PWD = 
-    HBNB_MYSQL_HOST = 'localhost'
-    HBNB_MYSQL_DB = the database name of your MySQL
-    
+    """
+    Class definition
+    """
     __engine = None
     __session = None
 
     def __init__(self):
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+        """
+        Constructor
+        """
 
-the engine must be linked to the MySQL database and user created before (hbnb_dev and hbnb_dev_db):
-# dialect: mysql
-# driver: mysqldb
-all of the following values must be retrieved via environment variables:
-MySQL user: HBNB_MYSQL_USER
-MySQL password: HBNB_MYSQL_PWD
-MySQL host: HBNB_MYSQL_HOST (here = localhost)
-MySQL database: HBNB_MYSQL_DB
-don’t forget the option pool_pre_ping=True when you call create_engine
-drop all tables if the environment variable HBNB_ENV is equal to test
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                        .format(HBNB_MYSQL_USER,
+                                        HBNB_MYSQL_PWD,
+                                        HBNB_MYSQL_HOST,
+                                        HBNB_MYSQL_DB),
+                                        pool_pre_ping=True)
+        
+        if HBNB_ENV == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+        def all(self, cls=None):
+            """
+            return a dictionary with all objects depending on class name
+            """
+            if cls is not None:
+                filter_dict = {}
+                for objects in self.__session.query(cls): # .filter(cls)
+                    key = objects.__class__.__name__ + '.' + objects.id
+                    filter_dict[key] = objects
+                return filter_dict
+            else:
+                all_dict = {}
+                for objects in self.__session:
+                    for key, value in objects.items():
+                        all_dict[key] = value
+                return all_dict
+
+        def new(self, obj):
+            """"add the object to the current database session"""
+            self.__session.add(obj)
+
+        def save(self):
+            """
+            Commit all changes of the current database session
+            """
+            self.__session.commit()
+
+        def delete(self, obj=None): 
+            """
+            delete from the current database session obj if not None
+            """
+            if obj is not None:
+                self.__session.delete(obj)
+            
+        def reload(self):
+            """create all tables in the database"""
+            # ¿?
+            Base.metadata.create_all(self.__engine)
+            # call_session is the factory
+            call_session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+            Session = scoped_session(call_session)
+            self.__sesion = Session
