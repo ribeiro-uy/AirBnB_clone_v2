8998c24157ea11167198de822f81090a325cc802
nicolasportela <2103@holbertonschool.com>
2021-03-30 02:58:16 +0000

    f***** reload fixed

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,6 +11,7 @@ from models.state import State
 from models.city import City
 from models.base_model import BaseModel, Base
 
+
 class DBStorage:
     """
     Class definition
@@ -30,54 +31,48 @@ class DBStorage:
         HBNB_ENV = getenv('HBNB_ENV')
 
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                        .format(HBNB_MYSQL_USER,
-                                        HBNB_MYSQL_PWD,
-                                        HBNB_MYSQL_HOST,
-                                        HBNB_MYSQL_DB),
-                                        pool_pre_ping=True)
+                                      .format(HBNB_MYSQL_USER,
+                                              HBNB_MYSQL_PWD,
+                                              HBNB_MYSQL_HOST,
+                                              HBNB_MYSQL_DB),
+                                      pool_pre_ping=True)
 
         if HBNB_ENV == 'test':
             Base.metadata.drop_all(self.__engine)
 
-        def all(self, cls=None):
-            """
-            return a dictionary with all objects depending on class name
-            """
-            if cls is not None:
-                filter_dict = {}
-                for objects in self.__session.query(cls): # .filter(cls)
-                    key = objects.__class__.__name__ + '.' + objects.id
-                    filter_dict[key] = objects
-                return filter_dict
-            else:
-                all_dict = {}
-                for objects in self.__session:
-                    for key, value in objects.items():
-                        all_dict[key] = value
-                return all_dict
+    def all(self, cls=None):
+        """return a dictionary with all objects depending on class name"""
+        if cls is not None:
+            filter_dict = {}
+            for objects in self.__session.query(cls):
+                key = objects.__class__.__name__ + '.' + objects.id
+                filter_dict[key] = objects
+            return filter_dict
+        else:
+            all_dict = {}
+            for objects in self.__session:
+                for key, value in objects.items():
+                    all_dict[key] = value
+            return all_dict
 
-        def new(self, obj):
-            """"add the object to the current database session"""
-            self.__session.add(obj)
+    def new(self, obj):
+        """"add the object to the current database session"""
+        self.__session.add(obj)
 
-        def save(self):
-            """
-            Commit all changes of the current database session
-            """
-            self.__session.commit()
+    def save(self):
+        """Commit all changes of the current database session"""
+        self.__session.commit()
 
-        def delete(self, obj=None):
-            """
-            delete from the current database session obj if not None
-            """
-            if obj is not None:
-                self.__session.delete(obj)
+    def delete(self, obj=None):
+        """delete from the current database session obj if not None"""
+        if obj is not None:
+            self.__session.delete(obj)
 
-        def reload(self):
-            """create all tables in the database"""
-            # ¿?
-            Base.metadata.create_all(self.__engine)
-            # call_session is the factory
-            call_session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-            Session = scoped_session(call_session)
-            self.__sesion = Session
+    def reload(self):
+        """create all tables in the database"""
+        # ¿?
+        Base.metadata.create_all(self.__engine)
+        # call_session is the factory
+        call_session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(call_session)
+        self.__sesion = Session
