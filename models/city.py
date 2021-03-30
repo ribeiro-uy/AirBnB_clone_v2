36412a028c54131b85b7f5f68dc477c5cfa2e9f5
nicolasportela <2103@holbertonschool.com>
2021-03-30 20:44:38 +0000

    classes updated, task 8

--- models/city.py
+++ models/city.py
@@ -4,6 +4,7 @@ import models
 from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -12,6 +13,7 @@ class City(BaseModel, Base):
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities")
 
     else:
         state_id = ""
--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 import models
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -11,8 +12,9 @@ class User(BaseModel, Base):
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
 
     else:
         email = ''
