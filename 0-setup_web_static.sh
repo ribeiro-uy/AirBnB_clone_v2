1d704c30b96626fbe4d12f0fa9bea6ddc2f17d25
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-04-16 10:43:32 -0300

    qué pretende usted de mi?

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 # Comment pal checker
 
-sudo apt -y update
+sudo apt update
 sudo apt -y install nginx
-mkdir -p /data
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo "<html>
@@ -18,9 +15,5 @@ echo "<html>
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 new_string="\\\n\tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\t}\n"
-check_exist=$(grep "hbnb_static" /etc/nginx/sites-available/default)
-if [[ -z $check_exist ]];
-then
-    sed -i "/server_name _/a $new_string" /etc/nginx/sites-available/default
-fi
+sed -i "/server_name _/a $new_string" /etc/nginx/sites-available/default
 sudo service nginx restart
