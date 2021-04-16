9902fabc03c611baf17ca363fb2386990cc15053
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-04-16 10:34:40 -0300

    remove if

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -38,9 +38,9 @@ sudo chown -hR ubuntu:ubuntu /data/
 
 new_string="\\\n\tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\t}\n"
 
-check_exist=$(grep "hbnb_static" /etc/nginx/sites-available/default)
-if [[ -z $check_exist ]];
-then
-    sed -i "/server_name _/a $new_string" /etc/nginx/sites-available/default
-fi
+# check_exist=$(grep "hbnb_static" /etc/nginx/sites-available/default)
+# if [[ -z $check_exist ]];
+# then
+sed -i "/server_name _/a $new_string" /etc/nginx/sites-available/default
+# fi
 sudo service nginx restart
